0f2453ef27d5936a255aa602a0f54d26ff4dc94c
trung300906 <taduytrung123@gmail.com>
2025-01-05 14:58:32 +0700

    learning-python

---
+++ about_array/chuoi.py
@@ -0,0 +1,8 @@
+s = 'team con me no djtme python'
+print(s)
+player = "tao deo biet "
+print(player)
+
+#syntax
+s = "I'm the god"
+print(s)
--- about_math_lb.py
+++ about_math_lb.py
@@ -6,3 +6,17 @@ print(math.ceil(a)) # làm tròn lên
 print(math.fabs(-a)) # lấy trị tuyệt đối
 print(math.sqrt(a)) # lấy căn
 print(math.gcd(math.ceil(a), 10)) # lấy gcd của phần làm tròn lên của a(là 4) với 10
+
+# about // and trunc
+
+a = 10
+b = -3
+print(a/b)
+print(a//b)
+print(math.trunc(a/b))
+
+#Toán tử //: Phép chia lấy phần nguyên sẽ trả về phần nguyên của kết quả chia và làm tròn xuống (floor division). Điều này có nghĩa là kết quả luôn hướng về số nguyên nhỏ hơn hoặc bằng kết quả thực của phép chia.
+
+#Hàm math.trunc(): Hàm này sẽ cắt bỏ phần thập phân và trả về phần nguyên của số. Nó không làm tròn xuống hay lên mà chỉ đơn giản là lấy phần nguyên của số.
+
+# chỉ ó hiệu quả với số
